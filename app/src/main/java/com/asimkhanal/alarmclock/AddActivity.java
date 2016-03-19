83caf83c174a77a8507a622b0faf7c1af2e5fa9d
Asim KHANAL <asim.khanal@vanderbilt.edu>
2016-03-19 15:15:56 -0500

    Added some services and stuff

--- app/src/main/java/com/asimkhanal/alarmclock/AddActivity.java
+++ app/src/main/java/com/asimkhanal/alarmclock/AddActivity.java
@@ -1,14 +1,65 @@
 package com.asimkhanal.alarmclock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class AddActivity extends AppCompatActivity {
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TextView alarmTextView;
+    private TimePicker alarmTimePicker;
+    private static AddActivity inst;
+    private String alarmText;
+
+    public static AddActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
         setTitle("Add an alarm");
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        alarmTextView = (TextView) findViewById(R.id.text_time);
+    }
+
+    public void onToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MyActivity", "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent myIntent = new Intent(AddActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(AddActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MyActivity", "Alarm Off");
+        }
+    }
+
+    public void setAlarmText(String alarmText) {
+        this.alarmText = alarmText;
+        alarmTextView.setText(alarmText);
     }
 }
